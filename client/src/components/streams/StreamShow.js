066f564ba56bdc3c08ac7cd0ca46cda7d5f8e4f3
Karan Dhanoya <kdhanoya94@gmail.com>
2018-12-11 13:24:22 +0000

    streaming added

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
 import {fetchStream} from '../actions'
+import flv from 'flv.js'
 
 
 class StreamShow extends Component {
 
-    componentDidMount(){
-        this.props.fetchStream(this.props.match.params.id)
+    constructor(props){
+        super(props)
+        this.videoRef = React.createRef()
     }
 
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id)
+        this.buildPlayer()
 
+    }
+
+    componentDidUpdate(){
+        this.buildPlayer()
+    }
+
+    componentWillUnmount(){
+        this.player.destroy()
+    }
+
+    buildPlayer = () =>{
+        if(this.player || !this.props.stream){
+            return
+        }
+
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${this.props.match.params.id}.flv`,
+
+        })
+
+        this.player.attachMediaElement(this.videoRef.current)
+        this.player.load()
+    }
 
     render() {
         if(!this.props.stream){
@@ -17,6 +46,7 @@ class StreamShow extends Component {
         }
         return (
             <div>
+                <video ref={this.videoRef} style={{width: "100%"}} controls={true}></video>
                 <h1>{this.props.stream.title}</h1>
                 <h5>{this.props.stream.description}</h5>
             </div>
